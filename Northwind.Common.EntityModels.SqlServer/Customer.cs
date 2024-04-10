2c88a0ff80a5721731a923adae0c5e5d2ff5a93c
Vy <nguyenhuutrongvy@gmail.com>
2024-04-10 11:27:58 +0700

    Storage

---
+++ Northwind.Common.EntityModels.SqlServer/Customer.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Serialization;
+
+namespace Packt.Shared;
+
+[Index("City", Name = "City")]
+[Index("CompanyName", Name = "CompanyName")]
+[Index("PostalCode", Name = "PostalCode")]
+[Index("Region", Name = "Region")]
+public partial class Customer
+{
+    [Key]
+    [Required]
+    [StringLength(5)]
+    [RegularExpression("[A-Z]{5}")]
+    public string CustomerId { get; set; } = null!;
+
+    [Required]
+    [StringLength(40)]
+    public string CompanyName { get; set; } = null!;
+
+    [StringLength(30)]
+    public string? ContactName { get; set; }
+
+    [StringLength(30)]
+    public string? ContactTitle { get; set; }
+
+    [StringLength(60)]
+    public string? Address { get; set; }
+
+    [StringLength(15)]
+    public string? City { get; set; }
+
+    [StringLength(15)]
+    public string? Region { get; set; }
+
+    [StringLength(10)]
+    public string? PostalCode { get; set; }
+
+    [StringLength(15)]
+    public string? Country { get; set; }
+
+    [StringLength(24)]
+    public string? Phone { get; set; }
+
+    [StringLength(24)]
+    public string? Fax { get; set; }
+
+    [InverseProperty("Customer")]
+    [XmlIgnore]
+    public virtual ICollection<Order> Orders { get; } = new List<Order>();
+
+    [ForeignKey("CustomerId")]
+    [InverseProperty("Customers")]
+    public virtual ICollection<CustomerDemographic> CustomerTypes { get; } = new List<CustomerDemographic>();
+}
---
+++ Northwind.WebApi/Program.cs
@@ -0,0 +1,88 @@
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Northwind.Common.DataContext.SqlServer;
+using Northwind.WebApi.Repositories;
+using Packt.Shared;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+using static System.Console;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseUrls("https://localhost:5002/");
+
+// Add services to the container.
+builder.Services.AddNorthwindContext();
+builder.Services
+    .AddControllers(options =>
+    {
+        WriteLine("Default output formatters:");
+        foreach (IOutputFormatter formatter in options.OutputFormatters)
+        {
+            OutputFormatter? mediaFormatter = formatter as OutputFormatter;
+            if (mediaFormatter == null)
+            {
+                WriteLine($"  {formatter.GetType().Name}");
+            }
+            else
+            {
+                // OutputFormatter class has SupportedMediaTypes
+                WriteLine("  {0}, Media types: {1}",
+                    arg0: mediaFormatter.GetType().Name,
+                    arg1: string.Join(", ", mediaFormatter.SupportedMediaTypes));
+            }
+        }
+    })
+    .AddXmlDataContractSerializerFormatters()
+    .AddXmlSerializerFormatters();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Northwind Service API", Version = "v1" });
+});
+
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = HttpLoggingFields.All;
+    options.RequestBodyLogLimit = 4096; // Default is 32k
+    options.ResponseBodyLogLimit = 4096; // Default is 32k
+});
+
+builder.Services
+    .AddHealthChecks()
+    .AddDbContextCheck<NorthwindContext>();
+
+var app = builder.Build();
+
+app.UseHttpLogging();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Northwind Service API Version 1");
+        c.SupportedSubmitMethods(new[] {
+            SubmitMethod.Get,
+            SubmitMethod.Post,
+            SubmitMethod.Put,
+            SubmitMethod.Delete });
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseHealthChecks(path: "/howdoyoufeel");
+
+app.UseMiddleware<SecurityHeaders>();
+
+app.MapControllers();
+
+app.Run();
